a20bfc17629fc9228f8aa7adbb89903184d0a473
AgustinRujana <87715028+Loesen-AgustinRujana@users.noreply.github.com>
2022-01-30 19:07:45 +0100

    Content Servicios Empresas

---
+++ content/serviciosEmpresas.js
@@ -0,0 +1,130 @@
+export const serviciosEmpresas = {
+  title: 'Bienvenido al servicio para empresas de Grupo EPS',
+  href: '/images/empresas/mainEmpresas.jpg',
+  cards: {
+    title: 'Cuidamos la salud de sus colaboradores',
+    content: [
+      {
+        title: 'Jornadas Médicas',
+        text: 'Tamizajes, exámenes de laboratorio, chequeos médicos, vacunación, optometría, certificados de salud y pruebas COVID-19. Implementación del Diario Anual de Salud en su empresa.',
+        style: '', //Otros estilos
+        color: '-blue-', //className={`bg-${color}-500`} Por ejemplo
+        href: '', //Referencia boton
+      },
+      {
+        title: 'Salud para su Empresa',
+        text: 'Planes de salud individuales o familiares accesibles, que se adaptan a las necesidades de sus colaboradores brindando un servicio médico integral y de calidad, atendido por los mejores profesionales del país.',
+        style: '', //Otros estilos
+        color: '-blue-', //className={`bg-${color}-500`} Por ejemplo
+        href: '', //Referencia boton
+      },
+      {
+        title: 'Riesgo Laboral',
+        text: 'Toda actividad económica genera riesgos laborales que requieren ser identificados y controlados para optimizar el funcionamiento de la empresa, mejorando las condiciones laborales de cada miembro de la compañía y evitando cualquier tipo de siniestralidad.',
+        style: '', //Otros estilos
+        color: '-blue-', //className={`bg-${color}-500`} Por ejemplo
+        href: '', //Referencia boton
+      },
+      {
+        title: 'Farmacia Empresarial',
+        text: 'Programa de paciente crónico, farmacia empresarial, envío de medicamentos a domicilio.',
+        style: '', //Otros estilos
+        color: '-blue-', //className={`bg-${color}-500`} Por ejemplo
+        href: '', //Referencia boton
+      },
+    ],
+  },
+  info: [
+    {
+      title: 'Jornadas Médicas',
+      text: `Las jornadas médicas sirven para establecer un diagnóstico individual y colectivo de la salud de la población de su empresa. 
+Nuestro objetivo es identificar enfermedades crónicas, así como factores de riesgo para disminuir tanto la siniestralidad de las mismas y las consecuencias de estas al no ser tratadas o no ser manejadas adecuadamente. 
+Estas jornadas tienen lugar una vez al año, en donde se realiza una evaluación física de sus colaboradores, así como la toma de exámenes de laboratorio y posterior evaluación de sus resultados. 
+Una vez obtenidos los mismos, pasamos a la etapa denominada DAS (Diario Anual de Salud) donde nuestros especialistas, hacen uso de su conocimiento médico para ayudar a los pacientes que presenten afecciones, alteraciones o riesgo de enfermedad, creando un programa especializado junto a su empresa y disminuyendo así, la siniestralidad.`,
+      style: '', //Otros estilos
+      color: '-blue-', //className={`bg-${color}-500`} Por ejemplo
+      href: '', //Referencia boton
+    },
+    {
+      title: 'Riesgo Laboral',
+      text: `Brindamos un servicio de asesoría para identificar y controlar de forma prioritaria los posibles riesgos laborales dentro de su empresa y/o sector económico. 
+Brindamos orientación sobre los beneficios de implementar medidas de salud ocupacional en su empresa. `,
+      style: '', //Otros estilos
+      color: '-blue-', //className={`bg-${color}-500`} Por ejemplo
+      href: '', //Referencia boton
+    },
+    {
+      title: 'Farmacia Empresarial',
+      text: `Luego de consulta con el médico internista, se coordina el envío mensual de los medicamentos que cada paciente crónico requiera, a su domicilio o lugar de trabajo. Esto permitirá tener un control sobre la entrega de medicamentos y por lo tanto, una baja significativa en los costos de farmacia para su empresa.`,
+      style: '', //Otros estilos
+      color: '-blue-', //className={`bg-${color}-500`} Por ejemplo
+      href: '', //Referencia boton
+    },
+  ],
+  plans: [
+    {
+      title: 'Plan Quiero Salud - Individual o familiar', //Escribir Descuentos en el componente
+      content: [
+				{ text: 'Medicina General', discount: '100%' },
+				{ text: 'Videconsulta Medicina General ', discount: '100%' },
+				{ text: 'Medicina domiciliaria', discount: '30%' },
+				{ text: 'Médico internista', discount: '80%' },
+				{ text: 'Cirujano', discount: '30%' },
+				{ text: 'Ginecología y obstetricia', discount: '80%' },
+				{ text: 'Traumatología y ortopedia', discount: '60%' },
+				{ text: 'Terapia física y rehabilitación', discount: '50%' },
+				{ text: 'Enfermería', discount: '100%' },
+				{ text: 'Otorrinolaringología', discount: '50%' },
+				{ text: 'Oftalmología', discount: '10%' },
+				{ text: 'Optometría', discount: '100%' },
+				{ text: 'Odontología', discount: '30%' },
+				{ text: 'Pruebas de laboratorio', discount: '10%' },
+				{ text: 'Rayos X', discount: '10%' },
+				{ text: 'Radiología', discount: '10%' },
+				{ text: 'Ultrasonido', discount: '10%' },
+				{ text: 'Farmacia', discount: '10%' },
+				{ text: 'Vacunación influenza', discount: '10%' },
+				{ text: 'Suplementos vitamínicos', discount: '10%' },
+				{ text: 'Gimnasios', discount: '10%' },
+				{ text: 'Primeros auxilios (curso pre grabado)', discount: '100%' },
+				{ text: 'Terapia física y rehabilitación (curso pre grabado)', discount: '100%' },
+				{ text: 'Manual para padres (cursos pre grabados)', discount: '100%' }
+			],
+      style: '', //Otros estilos
+      color: '-blue-', //className={`bg-${color}-500`} Por ejemplo
+      href: '', //Referencia boton
+    },
+    {
+      title: 'Plan Vida Plus- Individual o familiar', //Escribir Descuentos en el componente
+      content: [
+				{ text: 'Medicina General', discount: '100%' },
+				{ text: 'Videconsulta Medicina General ', discount: '100%' },
+				{ text: 'Medicina domiciliaria', discount: '100%' },
+				{ text: 'Médico internista', discount: '80%' },
+				{ text: 'Cirujano', discount: '30%' },
+				{ text: 'Ginecología y obstetricia', discount: '80%' },
+				{ text: 'Traumatología y ortopedia', discount: '60%' },
+				{ text: 'Terapia física y rehabilitación', discount: '50%' },
+				{ text: 'Enfermería', discount: '100%' },
+				{ text: 'Otorrinolaringología', discount: '50%' },
+				{ text: 'Oftalmología', discount: '30%' },
+				{ text: 'Optometría', discount: '100%' },
+				{ text: 'Odontología', discount: '30%' },
+				{ text: 'Pruebas de laboratorio', discount: '30%' },
+				{ text: 'Rayos X', discount: '30%' },
+				{ text: 'Radiología', discount: '30%' },
+				{ text: 'Ultrasonido', discount: '30%' },
+				{ text: 'Farmacia', discount: '20%' },
+				{ text: 'Vacunación influenza', discount: '20%' },
+				{ text: 'Suplementos vitamínicos', discount: '20%' },
+				{ text: 'Gimnasios', discount: '20%' },
+				{ text: 'Primeros auxilios (curso pre grabado)', discount: '100%' },
+				{ text: 'Terapia física y rehabilitación (curso pre grabado)', discount: '100%' },
+				{ text: 'Manual para padres (cursos pre grabados)', discount: '100%' }
+			],
+      style: '', //Otros estilos
+      color: '-blue-', //className={`bg-${color}-500`} Por ejemplo
+      href: '', //Referencia boton
+    },
+  ],
+};
