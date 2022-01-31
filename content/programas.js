3ed41258534d93f690b201ed525a5241b082a381
AgustinRujana <87715028+Loesen-AgustinRujana@users.noreply.github.com>
2022-01-31 23:17:35 +0100

    Content

--- content/programas.js
+++ content/programas.js
@@ -1,73 +1,298 @@
-export const programas = [
-  {
-    type: 'Maternidad y primeros años',
-    title: 'Programa control parental',
-    titleColor: 'Por enfermería',
-    mainColor: '#00c9ff',
-    image: '/images/programas/1',
-    boxImage:
-      'En esta maravillosa etapa ponemos a tu disposición a un grupo de profesionales en enfermería, salud mental, lactancia materna y terapia física con énfasis en piso pélvico para acompañarte en el proceso de gestación.',
-    objetives: {
-      des: 'Los objetivos del programa son:',
-      items: [
-        {
-          icon: '',
-          txt: 'Identificar e intervenir oportunamente el riesgo materno fetal',
-        },
-        {
-          icon: '',
-          txt: 'Educar y preparar a la pareja para el embarazo, el mantenimiento de la salud de la madre y el bebé, el nacimiento y la vida de su nuevo hijo.',
-        },
-        {
-          icon: '',
-          txt: 'Prevenir y detectar oportunamente alteraciones asociadas a la gestación.',
-        },
-      ],
-    },
-    activities: {
-      title: 'Actividades',
-      items: [
-        {
-          icon: '',
-          title: 'Consultas',
-          txt: 'Consulta individual y personalizada por profesionales en enfermería, lactancia materna y terapia física con énfasis en piso pélvico.',
-        },
-        {
-          icon: '',
-          title: 'Educación',
-          txt: 'Educación sobre aspectos propios de cada gestante y señales de alarma.',
-        },
-        {
-          icon: '',
-          title: 'Valoración',
-          txt: 'Valoración del progreso del crecimiento infantil y el peso materno.',
-        },
-        {
-          icon: '',
-          title: 'Orientación',
-          txt: 'Orientación sobre planificación familiar',
-        },
-        {
-          icon: '',
-          title: 'Asesoría',
-          txt: 'Asesoría en lactancia materna.',
-        },
-        {
-          icon: '',
-          title: 'Remisíon de servicio',
-          txt: 'Remisión a servicio Remisión a servicio de: vacunación, odontología, psicología y nutrición, según necesidad y no son cubiertas por el programa.',
-        },
-        {
-          icon: '',
-          title: 'Remisíon a curso',
-          txt: 'Remisión a curso de preparación para la maternidad y la paternidad.',
-        },
-        {
-          icon: '',
-          title: 'Preparacion',
-          txt: 'Preparación para la atención del parto/cesárea y cuidados postparto',
-        },
-      ],
-    },
+export const programas = {
+  maternidad: {
+    title: 'Maternidad y primeros años',
+    color: 'bg-sky-400',
+    cards: [
+      {
+        id: 1,
+        title: 'Programa Control Prenatal - Primer Trimestre',
+        content:
+          'Estamos contigo en esta maravillosa etapa. Nuestro equipo de profesionales te brindará asesoría y acompañamiento, desde el momento en que te enteras de tu embarazo. Este programa incluye todo lo que necesitas para transitar la gestación de tu bebé.',
+        image: '/images/programas/1.png',
+        info: [
+          { name: 'Medicina general', 1: '100%', 2: '100%' },
+          { name: 'Videoconsulta medicina general', 1: '100%', 2: '100%' },
+          { name: 'Médico internista', 1: '80%', 2: '80%' },
+          { name: 'Ginecología y obstetricia', 1: '80%', 2: '80%' },
+          { name: 'Terapia física y rehabilitación', 1: '50%', 2: '50%' },
+          { name: 'Enfermería', 1: '100%', 2: '100%' },
+          { name: 'Asesor lactancia (Lic. enfermería)', 1: '100%', 2: '100%' },
+          { name: 'Odontología', 1: '30%', 2: '30%' },
+          { name: 'Orientación en planif. familiar', 1: '100%', 2: '100%' },
+          { name: 'Laboratorios "Diario Anual de Salud"', 1: '50%', 2: '100%' },
+          { name: 'Pruebas de laboratorio', 1: '10%', 2: '30%' },
+          { name: 'Pruebas Covid', 1: '10%', 2: '30%' },
+          { name: 'Ultrasonido', 1: '10%', 2: '30%' },
+          { name: 'Farmacia', 1: '10%', 2: '20%' },
+          { name: 'Monitoreo Fetal ', 1: '10%', 2: '20%' },
+          { name: 'Nutrición', 1: '60%', 2: '80%' },
+          {
+            name: 'Manual de padres - Vídeos pregrabados.',
+            1: '100%',
+            2: '100%',
+          },
+          {
+            name: 'Terapia física y rehabilitación - Vídeos pregrabados.',
+            1: '100%',
+            2: '100%',
+          },
+        ],
+      },
+      {
+        id: 1,
+        title: 'Programa Control Prenatal - Segundo Trimestre',
+        content:
+          'Estamos contigo en esta maravillosa etapa. Nuestro equipo de profesionales te brindará asesoría y acompañamiento, desde el momento en que te enteras de tu embarazo. Este programa incluye todo lo que necesitas para transitar la gestación de tu bebé.',
+        image: '/images/programas/1.png',
+        info: [
+          { name: 'Medicina general', 1: '100%', 2: '100%' },
+          { name: 'Videoconsulta medicina general', 1: '100%', 2: '100%' },
+          { name: 'Médico internista', 1: '80%', 2: '80%' },
+          { name: 'Ginecología y obstetricia', 1: '80%', 2: '80%' },
+          { name: 'Terapia física y rehabilitación', 1: '50%', 2: '50%' },
+          { name: 'Enfermería', 1: '100%', 2: '100%' },
+          { name: 'Orientación en planif. familiar', 1: '100%', 2: '100%' },
+          { name: 'Laboratorios "Diario Anual de Salud"', 1: '50%', 2: '100%' },
+          { name: 'Pruebas de laboratorio', 1: '10%', 2: '30%' },
+          { name: 'Pruebas Covid', 1: '10%', 2: '30%' },
+          { name: 'Ultrasonido', 1: '10%', 2: '30%' },
+          { name: 'Farmacia', 1: '10%', 2: '20%' },
+          { name: 'Monitoreo Fetal ', 1: '10%', 2: '20%' },
+          { name: 'Nutrición', 1: '60%', 2: '80%' },
+          {
+            name: 'Manual de padres - Vídeos pregrabados.',
+            1: '100%',
+            2: '100%',
+          },
+          {
+            name: 'Terapia física y rehabilitación - Vídeos pregrabados.',
+            1: '100%',
+            2: '100%',
+          },
+        ],
+      },
+      {
+        id: 1,
+        title: 'Programa Control Prenatal - Tercer Trimestre',
+        content:
+          'Estamos contigo en esta maravillosa etapa. Nuestro equipo de profesionales te brindará asesoría y acompañamiento, desde el momento en que te enteras de tu embarazo. Este programa incluye todo lo que necesitas para transitar la gestación de tu bebé.',
+        image: '/images/programas/1.png',
+        info: [
+          { name: 'Medicina general', 1: '100%', 2: '100%' },
+          { name: 'Videoconsulta medicina general', 1: '100%', 2: '100%' },
+          { name: 'Médico internista', 1: '80%', 2: '80%' },
+          { name: 'Ginecología y obstetricia', 1: '80%', 2: '80%' },
+          { name: 'Pediatría', 1: '80%', 2: '80%' },
+          { name: 'Terapia física y rehabilitación', 1: '50%', 2: '50%' },
+          { name: 'Enfermería', 1: '100%', 2: '100%' },
+          { name: 'Dermatología', 1: '60%', 2: '60%' },
+          { name: 'Psicología', 1: '10%', 2: '30%' },
+          { name: 'Orientación en planif. familiar ', 1: '100%', 2: '100%' },
+          { name: 'Laboratorios "Diario Anual de Salud"', 1: '50%', 2: '100%' },
+          { name: 'Pruebas de laboratorio', 1: '10%', 2: '30%' },
+          { name: 'Pruebas Covid', 1: '10%', 2: '30%' },
+          { name: 'Ultrasonido', 1: '10%', 2: '30%' },
+          { name: 'Farmacia', 1: '10%', 2: '20%' },
+          { name: 'DIU', 1: '10%', 2: '20%' },
+          { name: 'Anticonceptivos', 1: '10%', 2: '20%' },
+          { name: 'Parto', 1: '10%', 2: '20%' },
+          { name: 'Cesarea', 1: '10%', 2: '20%' },
+          { name: 'Monitoreo Fetal ', 1: '10%', 2: '20%' },
+          { name: 'Nutrición', 1: '60%', 2: '80%' },
+          {
+            name: 'Manual de padres - Vídeos pregrabados.',
+            1: '100%',
+            2: '100%',
+          },
+          {
+            name: 'Terapia física y rehabilitación - Vídeos pregrabados.',
+            1: '100%',
+            2: '100%',
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: 'Programa Post Parto Saludable',
+        content:
+          'También te acompañamos luego del parto. Con este programa, obtendrás acompañamiento y asistencia de médicos, psicólogos, nutriólogos, dermatólogos y otros profesionales de la salud durante los primeros 40 días posteriores al nacimiento de tu bebé.',
+        image: '/images/programas/2.png',
+        info: [
+          { name: 'Ginecología y obstetricia', 1: '80%', 2: '80%' },
+          { name: 'Terapia física y rehabilitación', 1: '50%', 2: '50%' },
+          { name: 'Dermatología', 1: '60%', 2: '60%' },
+          { name: 'Psicología', 1: '10%', 2: '30%' },
+          { name: 'Laboratorios "Diario Anual de Salud"', 1: '50%', 2: '100%' },
+          { name: 'Pruebas de laboratorio', 1: '10%', 2: '30%' },
+          { name: 'Rayos X', 1: '10%', 2: '30%' },
+          { name: 'Ultrasonido', 1: '10%', 2: '30%' },
+          { name: 'Farmacia', 1: '10%', 2: '20%' },
+          { name: 'Vacunación influenza', 1: '10%', 2: '20%' },
+          { name: 'Suplementos vitaminicos', 1: '10%', 2: '20%' },
+          { name: 'Nutrición', 1: '60%', 2: '80%' },
+          { name: 'Gimnasios', 1: '10%', 2: '20%' },
+        ],
+      },
+      {
+        id: 3,
+        title: 'Programa Primeros Pasos',
+        content:
+          'Sus primeros pasos, son lo más importante. Con este programa, tu bebé recibirá la atención médica, vacunas y chequeos de rutina necesarios para atravesar su primer año de vida.',
+        image: '/images/programas/3.png',
+        info: {},
+      },
+      {
+        id: 4,
+        title: 'Progama Kinder',
+        content:
+          'Tu pequeño está empezando a crecer, con el programa kinder para que tendrá acceso a consultas pediátricas, vacunas y chequeos de rutina necesarios para tener un desarrollo saludable y una primera infancia feliz.',
+        image: '/images/programas/4.png',
+        info: {},
+      },
+    ],
   },
-];
+  infancia: {
+    title: 'Infancia y adolescencia',
+    color: 'bg-teal-400',
+    cards: [
+      {
+        id: 5,
+        title: 'Programa niños',
+        content:
+          'Seguimos cuidando de tu familia. Con el programa niños, garantizamos atención pediátrica, vacunación, psicología, oftalmología, kit de emergencia y todos los cuidados que tu hijo necesita para seguir creciendo saludable y feliz.',
+        image: '/images/programas/5.png',
+        info: {},
+      },
+      {
+        id: 6,
+        title: 'Programa pre adolescencia',
+        content:
+          'Seguimos cuidando de tu familia. Tu hijo está ingresando en una etapa muy importante de su desarrollo. Con el programa preadolescencia recibirá la atención médica, psicológica y cuidados profesionales necesarios para seguir creciendo saludable y feliz.',
+        image: '/images/programas/6.png',
+        info: {},
+      },
+      {
+        id: 7,
+        title: 'Programa adolescencia',
+        content:
+          'Seguimos cuidando de tu familia. Tu hijo está atravesando una etapa muy importante de su desarrollo. Con el programa adolescencia recibirá la atención médica, psicológica y cuidados profesionales necesarios para convertirse en un adulto saludable y feliz.',
+        image: '/images/programas/6.png',
+        info: {},
+      },
+    ],
+  },
+  adultos: {
+    title: 'Cuidados Especiales',
+    color: 'bg-fuchsia-700',
+    cards: [
+      {
+        id: 8,
+        title: 'Programa diabetes',
+        content:
+          'Junto a nuestros profesionales, te ayudamos a mejorar tu calidad de vida con un buen conocimiento y control de la enfermedad, y a prevenir complicaciones de la diabetes relacionadas con el corazón y el resto del sistema circulatorio, los ojos, los riñones, los nervios y la piel.',
+        image: '/images/programas/7.png',
+        info: {},
+      },
+      {
+        id: 9,
+        title: 'Programa de Hipertensión Arterial',
+        content:
+          'Nuestro equipo de profesionales, te brindará apoyo y educación en habilidades de autocuidado para prevenir o aplazar las complicaciones que se puedan generar a partir de la enfermedad y así mejorar tu calidad de vida.',
+        image: '/images/programas/8.png',
+        info: {},
+      },
+      {
+        id: 10,
+        title: 'Programa dislipemia',
+        content:
+          'Nuestro equipo de profesionales, te apoyará y acompañará en el proceso a través del cual mejorarás tu calidad de vida. Empieza hoy tu camino: chequeos médicos, dieta saludable con nutriólogos, descuento en gimnasios y mucho más.',
+        image: '/images/programas/9.png',
+        info: {},
+      },
+      {
+        id: 11,
+        title: 'Programa cardiopatías',
+        content:
+          'A través de este programa, nuestro equipo multidisciplinario, interviene a aquellos pacientes con insuficiencia cardíaca secundaria o enfermedad cardiovascular, brindando los chequeos médicos, acompañamientos, revisiones y cuidados que el tratamiento requiere.',
+        image: '/images/programas/9.png',
+        info: {},
+      },
+      {
+        id: 12,
+        title: 'Programa Asma',
+        content:
+          'El programa asma, se centra en el tratamiento de la afección y acompañamiento del paciente. Nuestro neumólogo, así como nuestro equipo multidisciplinario de profesionales, trabajarán por mejorar tu calidad de vida.',
+        image: '/images/programas/9.png',
+        info: {},
+      },
+      {
+        id: 13,
+        title: 'Programa "Contigo" Cuidados paliativos',
+        content:
+          'Este programa está diseñado para procurar tu bienestar y el de tu familia, a través de un equipo especializado en el cuidado y atención de las personas que se encuentran con enfermedades crónicas avanzadas, progresivas o incurables, brindándoles un acompañamiento cercano.',
+        image: '/images/programas/9.png',
+        info: {},
+      },
+    ],
+  },
+  acompanamiento: {
+    title: 'Hombres',
+    color: 'bg-amber-400',
+    cards: [
+      {
+        id: 14,
+        title: 'Hombres - Programa génesis 18 a 40 años',
+        content:
+          'Estamos junto a ti. Pensado para hombres de 18 a 40 años, el programa génesis, brinda descuentos para los más completos servicios de salud física y mental, médicos especialistas, gimnasios ¡Y mucho más! Vive una vida saludable junto a Grupo EPS.',
+        image: '/images/programas/12.png',
+        info: {},
+      },
+      {
+        id: 15,
+        title: 'Hombres - Programa vitalidad 40 a 60 años',
+        content:
+          'Estamos junto a ti. Pensado para hombres de 40 a 60 años, el programa vitalidad, brinda descuentos para los más completos servicios de salud física y mental, médicos especialistas, gimnasios ¡Y mucho más! Vive una vida saludable junto a Grupo EPS.',
+        image: '/images/programas/13.png',
+        info: {},
+      },
+      {
+        id: 16,
+        title: 'Hombres - Programa vida plena 60 años en adelante',
+        content:
+          'Estamos junto a ti. Pensado para hombres de 60 años en adelante, el programa vida plena te acompaña. Obtén descuentos en los más completos servicios de salud física y mental, médicos especialistas, gimnasios ¡Y mucho más! Vive una vida plena junto a Grupo EPS.',
+        image: '/images/programas/14.png',
+        info: {},
+      },
+    ],
+  },
+  mujeres: {
+    title: 'Mujeres',
+    color: 'bg-lime-500',
+    cards: [
+      {
+        id: 17,
+        title: 'Mujeres - Programa young 18 a 40 años',
+        content:
+          'Estamos junto a ti. Pensado para mujeres de 18 a 40 años, el programa young, brinda descuentos para los más completos servicios de salud física y mental, médicos especialistas, gimnasios ¡Y mucho más! Vive una vida saludable junto a Grupo EPS.',
+        image: '/images/programas/15.png',
+        info: {},
+      },
+      {
+        id: 18,
+        title: 'Mujeres - Programa vitalidad 40 a 60 años',
+        content:
+          'Estamos junto a ti. Pensado para mujeres de 40 a 60 años, el programa vitalidad, brinda descuentos para los más completos servicios de salud física y mental, médicos especialistas, gimnasios ¡Y mucho más! Vive una vida saludable junto a Grupo EPS.',
+        image: '/images/programas/16.png',
+        info: {},
+      },
+      {
+        id: 19,
+        title: 'Mujeres - Programa vida plena 60 años en adelante',
+        content:
+          'Estamos junto a ti. Pensado para mujeres de 60 años en adelante, el programa vida plena te acompaña. Obtén descuentos en los más completos servicios de salud física y mental, médicos especialistas, gimnasios ¡Y mucho más! Vive una vida plena junto a Grupo EPS.',
+        image: '/images/programas/17.png',
+        info: {},
+      },
+    ],
+  },
+};
